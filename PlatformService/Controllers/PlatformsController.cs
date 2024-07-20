841513be026fc39c4d318db392cf25ce81b8ae80
Lucas Richard Garcia Tinoco <lucas.tinoco@hotmail.com>
2024-07-20 19:12:58 -0300

    feat: add initial MVC structure

---
+++ PlatformService/Controllers/PlatformsController.cs
@@ -0,0 +1,19 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using PlatformService.Data;
+using PlatformService.DTOs;
+
+namespace PlatformService.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class PlatformsController(IPlatformRepository repository, IMapper mapper) : ControllerBase
+{
+    [HttpGet]
+    public ActionResult<IEnumerable<PlatformReadDto>> GetPlatforms()
+    {
+        Console.WriteLine("--> Getting Platforms...");
+        var platforms = repository.GetAllPlatforms();
+        return Ok(mapper.Map<IEnumerable<PlatformReadDto>>(platforms));
+    }
+}
---
+++ PlatformService/Data/PlatformRepository.cs
@@ -0,0 +1,29 @@
+﻿using PlatformService.Models;
+
+namespace PlatformService.Data;
+
+public class PlatformRepository(AppDbContext context) : IPlatformRepository
+{
+    public bool SaveChanges()
+    {
+        return context.SaveChanges() >= 0;
+    }
+
+    public IEnumerable<Platform> GetAllPlatforms()
+    {
+        return context.Platforms.ToList();
+    }
+
+    public Platform GetPlatformById(int id)
+    {
+        return context.Platforms.FirstOrDefault(p => p.Id == id);
+    }
+
+    public void CreatePlatform(Platform platform)
+    {
+        ArgumentNullException.ThrowIfNull(platform);
+
+        context.Platforms.Add(platform);
+        SaveChanges();
+    }
+}
