75adfcc441b5a10d0202458db38affd4ec2560cf
Lucas Richard Garcia Tinoco <lucas.tinoco@hotmail.com>
2024-07-21 16:17:12 -0300

    ci: add kubernetes

--- PlatformService/Controllers/PlatformsController.cs
+++ PlatformService/Controllers/PlatformsController.cs
@@ -2,6 +2,7 @@
 using Microsoft.AspNetCore.Mvc;
 using PlatformService.Data;
 using PlatformService.DTOs;
+using PlatformService.Models;
 
 namespace PlatformService.Controllers;
 
@@ -15,5 +16,24 @@ public class PlatformsController(IPlatformRepository repository, IMapper mapper)
         Console.WriteLine("--> Getting Platforms...");
         var platforms = repository.GetAllPlatforms();
         return Ok(mapper.Map<IEnumerable<PlatformReadDto>>(platforms));
+    }
+
+    [HttpGet("{id:int}", Name = "GetPlatformById")]
+    public ActionResult<PlatformReadDto> GetPlatformById(int id)
+    {
+        Console.WriteLine("--> Getting Platform By Id...");
+        var platform = repository.GetPlatformById(id);
+        return platform is not null ? Ok(mapper.Map<PlatformReadDto>(platform)) : NotFound();
+    }
+
+    [HttpPost]
+    public ActionResult<PlatformReadDto> CreatePlatform([FromBody] PlatformCreateDto platformCreateDto)
+    {
+        Console.WriteLine("--> Creating Platform...");
+        var platformModel = mapper.Map<Platform>(platformCreateDto);
+        repository.CreatePlatform(platformModel);
+        repository.SaveChanges();
+        var platformReadDto = mapper.Map<PlatformReadDto>(platformModel);
+        return CreatedAtRoute(nameof(GetPlatformById), new { platformReadDto.Id }, platformReadDto);
     }
 }
--- PlatformService/Data/IPlatformRepository.cs
+++ PlatformService/Data/IPlatformRepository.cs
@@ -6,6 +6,6 @@ public interface IPlatformRepository
 {
     bool SaveChanges();
     IEnumerable<Platform> GetAllPlatforms();
-    Platform GetPlatformById(int id);
-    void CreatePlatform(Platform platform); 
+    Platform? GetPlatformById(int id);
+    void CreatePlatform(Platform platform);
 }
